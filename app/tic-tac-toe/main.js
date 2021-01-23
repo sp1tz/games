117ba4a843eaf82d490ed6e8640be20c293ea690
sp1tz <42953063+sp1tz@users.noreply.github.com>
2021-01-23 18:20:20 -0500

    TTT main.js.

---
+++ app/tic-tac-toe/main.js
@@ -0,0 +1,21 @@
+'use strict';
+(function() {
+  let diffHandler,
+      multiPlay,
+      soloPlay;
+
+  function changeVisibilty() {
+    diffHandler.style.visibility = multiPlay.checked ? 'hidden' : 'visible';
+  }
+
+  window.addEventListener('load', () => {
+    diffHandler = document.getElementById('difficultySection');
+    multiPlay = document.getElementById('2player');
+    soloPlay = document.getElementById('1player');
+
+    changeVisibilty();
+
+    multiPlay.addEventListener('click', changeVisibilty);
+    soloPlay.addEventListener('click', changeVisibilty);
+  });
+}());
